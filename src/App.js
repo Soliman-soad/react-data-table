cc13381ebe44eec6f44cb217ad0c85f315c0d90d
Soliman-soad <mdsolimansoad@gmail.com>
2022-12-11 00:49:21 +0600

    data added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import DataTable from 'react-data-table-component';
+import { useState } from 'react';
 
 function App() {
+  const [data,setData] =useState([]);
+  const [loading,setLoading]= useState(false)
+  useState(()=>{
+    fetch('https://retoolapi.dev/4547z4/data')
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+    })
+  },[data])
+
+  const columns = [
+    {
+        name: 'Id',
+        selector: row => row.id,
+        sortable: true,
+    },
+    {
+        name: 'First Name',
+        selector: row => row.first_name,
+        sortable: true,
+    },
+    {
+        name: 'Last Name',
+        selector: row => row.last_name,
+        sortable: true,
+    },
+    {
+        name: 'Full name',
+        selector: row => row.first_name+" " + row.last_name,
+        sortable: true,
+    },
+];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataTable
+            columns={columns}
+            data={data}
+        />
     </div>
   );
 }
